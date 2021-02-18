40926dc7169ce1798e5c18c802754b96afc9db11
MaxDev11 <sshadowp@protonmail.com>
2021-02-18 20:16:49 +0600

    Add algorithms project and Fix some issues

---
+++ projects/algorithms/algorithms.js
@@ -0,0 +1,34 @@
+// Factorial
+
+function computeFactorial() {
+    let factorialInput = +(prompt("Enter the factorial"));
+    let factorialOutput = 1;
+
+    for (let i = 2; i <= factorialInput; i++) {
+        factorialOutput *= i;
+    }
+    
+    alert(factorialOutput.toString());
+}
+
+// Factorial (Recursive function)
+
+// function computeFactorialR(factorialInput) {
+//     return factorialInput > 1 ? 
+//         factorialInput * computeFactorialR(factorialInput - 1) : 
+//         1;
+// }
+
+// Fibonacci
+
+function computeFibonacci() {
+    let fibonacciInput = +(prompt("Enter the fibonacci"));
+    let fibonacciOutput = [];
+
+    for (let i = 0; i < fibonacciInput; i++) {
+        fibonacciOutput[i];
+        console.log(fibonacciOutput[i]);
+    }
+    
+    alert("Look in the console");
+}
