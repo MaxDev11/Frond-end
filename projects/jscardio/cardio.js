d8980e31a689db98385c82c5bf30edeaacd79da0
MaxDev11 <sshadowp@protonmail.com>
2021-02-28 22:12:10 +0600

    Add JS cardio project

---
+++ projects/jscardio/cardio.js
@@ -0,0 +1,61 @@
+// Reverse a string
+function reverseString(str) {
+
+    // 1. let arr = str.split('');
+    // let newArr = [];
+    // for (let i = arr.length - 1; i >= 0; i--) {
+    //     newArr.push(arr[i]);
+    // }
+
+    // 2. str.split('').reverse().join('');
+    
+    // 3. let newArr = arr.reduce((acc, x) => [x, ...acc], []);
+
+}
+
+
+// Validate a palindrome
+// palindrome "racecar" === true
+function isPalindrome(str) {
+        // if (str.split('').reverse().join('') === str) {
+        //     console.log('true');
+        // } else {
+        //     console.log('false');
+        // }
+}
+
+
+// Reverse an integer
+function reverseInt(int) {
+    // const s = int.toString().split('').reverse().join('');
+    // return parseInt(s) * Math.sign(int);
+}
+
+
+// Capitalize first letters of each word
+function capitalizeLetters(str) {
+    // let arr = str.split('');
+    // return arr.map((e) => e.charAt(0).toUpperCase());
+}
+
+// FizzBuzz
+// Given an array from 1 to n
+// Replace each number divisable by 3 with Fizz
+// Replace each number divisable by 5 with Buzz
+// Replace each number divisable by 15 with FizzBuzz
+function FizzBuzz(arr) {
+
+    // 1. for (let i = 0; i <= arr.length; i++) {
+    //     if (arr[i] % 15 === 0) arr[i] = 'FizzBuzz';
+    //     if (arr[i] % 3 === 0) arr[i] = 'Fizz'; 
+    //     if (arr[i] % 5 === 0) arr[i] = 'Buzz';
+    // }
+    // let newArr = arr.map((e) => {
+    //     if (e % 15 === 0) e = 'FizzBuzz';
+    //     if (e % 3 === 0) e = 'Fizz'; 
+    //     if (e % 5 === 0) e = 'Buzz';
+    // });
+    // console.log(newArr);
+
+}
+let a = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15];
