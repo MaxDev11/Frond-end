f0dccde7c756ff3ca98ffd35baa716d551218feb
MaxDev11 <sshadowp@protonmail.com>
2021-03-01 12:14:12 +0600

    Finish first session in jscardio

--- projects/jscardio/cardio.js
+++ projects/jscardio/cardio.js
@@ -1,61 +1,63 @@
-// Reverse a string
+const output = (o) => console.log(o);
+
+
+/* FIRST CARDIO SESSION (Traversy media JS cardio sessions) */
+
+
 function reverseString(str) {
-
-    // 1. let arr = str.split('');
-    // let newArr = [];
-    // for (let i = arr.length - 1; i >= 0; i--) {
-    //     newArr.push(arr[i]);
-    // }
-
-    // 2. str.split('').reverse().join('');
-    
-    // 3. let newArr = arr.reduce((acc, x) => [x, ...acc], []);
-
+    // 1. str.split('').reverse().join('');
+    return str.split('').reduce((acc, x) => [x, ...acc], []);
 }
 
-
-// Validate a palindrome
-// palindrome "racecar" === true
 function isPalindrome(str) {
-        // if (str.split('').reverse().join('') === str) {
-        //     console.log('true');
-        // } else {
-        //     console.log('false');
-        // }
+    return str.split('').reverse().join('') === str ? 
+           output('true') : output('false');
 }
 
-
-// Reverse an integer
 function reverseInt(int) {
-    // const s = int.toString().split('').reverse().join('');
-    // return parseInt(s) * Math.sign(int);
+    const s = int.toString().split('').reverse().join('');
+    return parseInt(s) * Math.sign(int);
 }
 
-
-// Capitalize first letters of each word
 function capitalizeLetters(str) {
-    // let arr = str.split('');
-    // return arr.map((e) => e.charAt(0).toUpperCase());
+    return str.toLowerCase()
+              .split(' ')
+              .map((e) => e[0].toUpperCase() 
+                        + e.substring(1))
+              .join(' ');    
 }
 
-// FizzBuzz
-// Given an array from 1 to n
-// Replace each number divisable by 3 with Fizz
-// Replace each number divisable by 5 with Buzz
-// Replace each number divisable by 15 with FizzBuzz
-function FizzBuzz(arr) {
+function commonChar(str) {
+    let chMap = {},
+          maxN = 0, maxCh = '';
 
-    // 1. for (let i = 0; i <= arr.length; i++) {
-    //     if (arr[i] % 15 === 0) arr[i] = 'FizzBuzz';
-    //     if (arr[i] % 3 === 0) arr[i] = 'Fizz'; 
-    //     if (arr[i] % 5 === 0) arr[i] = 'Buzz';
-    // }
-    // let newArr = arr.map((e) => {
-    //     if (e % 15 === 0) e = 'FizzBuzz';
-    //     if (e % 3 === 0) e = 'Fizz'; 
-    //     if (e % 5 === 0) e = 'Buzz';
-    // });
-    // console.log(newArr);
+    str.split('').map((ch) => chMap[ch] ? chMap[ch]++ : chMap[ch] = 1);
 
+    for (let ch in chMap) {
+        if (chMap[ch] > maxN) {
+            maxN = chMap[ch];
+            maxCh = ch;
+        }
+    }
+
+    return maxCh;
 }
-let a = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15];
+
+function flatten2D(arr) {
+    return arr.map((e) => e.join('')).join('').split('');
+}
+
+function flattenINF(arr) {
+    return arr.map((e) => e.flat()).join(',').split(',');
+}
+
+function fizzBuzz(arr) {
+    for (let i = 0; i <= arr.length; i++) {
+        if (arr[i] % 15 === 0) arr[i] = 'FizzBuzz';
+        if (arr[i] % 3 === 0) arr[i] = 'Fizz'; 
+        if (arr[i] % 5 === 0) arr[i] = 'Buzz';
+    }
+}
+
+
+/* SECOND CARDIO SESSION (Traversy media JS cardio sessions) */
