5640585e6a8781b9c4c8a51b056079db37056775
MaxDev11 <sshadowp@protonmail.com>
2021-03-02 12:47:16 +0600

    Add Second Js cardio session

--- projects/jscardio/cardio.js
+++ projects/jscardio/cardio.js
@@ -4,10 +4,8 @@ const output = (o) => console.log(o);
 /* FIRST CARDIO SESSION (Traversy media JS cardio sessions) */
 
 
-function reverseString(str) {
-    // 1. str.split('').reverse().join('');
-    return str.split('').reduce((acc, x) => [x, ...acc], []);
-}
+const reverseString = (s) => s.split('')
+                              .reduce((acc, x) => [x, ...acc], []);
 
 function isPalindrome(str) {
     return str.split('').reverse().join('') === str ? 
@@ -43,21 +41,66 @@ function commonChar(str) {
     return maxCh;
 }
 
-function flatten2D(arr) {
-    return arr.map((e) => e.join('')).join('').split('');
-}
-
-function flattenINF(arr) {
-    return arr.map((e) => e.flat()).join(',').split(',');
-}
-
-function fizzBuzz(arr) {
-    for (let i = 0; i <= arr.length; i++) {
-        if (arr[i] % 15 === 0) arr[i] = 'FizzBuzz';
-        if (arr[i] % 3 === 0) arr[i] = 'Fizz'; 
-        if (arr[i] % 5 === 0) arr[i] = 'Buzz';
-    }
+function fizzBuzz(a) {
+    a.map((e) => {
+        return e % 15 === 0 ? 'FizzBuzz' :
+               e % 3 === 0 ? 'Fizz' :
+               e % 5 === 0 ? 'Buzz' : e;
+    });
 }
 
 
 /* SECOND CARDIO SESSION (Traversy media JS cardio sessions) */
+
+
+function longestWord(s) {
+    const words = s.toLowerCase().match(/[a-z0-9]+/g);
+    const sorted = words.sort((a, b) => b.length - a.length);
+    const longestWord = sorted.filter((word) => {
+        return word.length === sorted[0].length;
+    });
+
+    return longestWord.length === 1 ? longestWord[0] : longestWord;
+}
+
+// ex chunkArray([1, 2, 3, 4, 5, 6], 2) === [[1, 2, 3], [4, 5, 6]]
+function chunkArray(arr, len) {
+    const chunkedArr = [];
+    let i = 0;
+    while (i < arr.length) {
+        chunkedArr.push(arr.slice(i, i + len));
+        i += len;
+    }
+
+    return chunkedArr;
+}
+
+const flatten2D = (a) => a.map((e) => e.join(''))
+                          .join('').split('');
+
+const flattenINF = (a) => a.map((e) => e.flat())
+                           .join(',').split(',');
+                    
+function isAnagram(s1, s2) {
+    return formatStr(s1) === formatStr(s2);
+}
+function formatStr(str) {
+    return str.replace(/[^\w]/g, '').toLowerCase().split('')
+              .sort().join('');
+}
+
+function letterChanges(s) {
+    let newStr = s.replace(/[a-z]/gi, (ch) => {
+        if (ch === 'z' || ch === 'Z') {
+            return 'a';
+        } else {
+            return String.fromCharCode(ch.charCodeAt() + 1);
+        }
+    });
+
+    newStr = newStr.replace(/a|e|i|o|u/gi, (vow) => {
+        return vow.toUpperCase();
+    });
+
+    return newStr;
+}
