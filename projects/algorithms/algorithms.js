6efcc9676583812950be22ffdbc701954d07c6be
MaxDev11 <sshadowp@protonmail.com>
2021-03-06 21:56:50 +0600

    Change algorithms project

--- projects/algorithms/algorithms.js
+++ projects/algorithms/algorithms.js
@@ -1,48 +1,90 @@
- // Source repository: https://github.com/trekhleb/javascript-algorithms
+// NodeJS (prompt-sync)
+// Source repositories:
+// https://github.com/trekhleb/javascript-algorithms
+// https://github.com/egonschiele/grokking_algorithms
 
-const d = document;
 
-// Factorial
 
-const computeFactorial = () => {
-    let factorialInput = +(d.querySelector("#factorial").value);
-    let factorialOutput = 1;
-    let facOutput = d.querySelector("#fac_output");
+/* GROKKING ALGORITHMS */
+// Chapter 1
+// Binary search, simple search (Algorithms)
+// Big O notation (Tells you how fast an algorithm is)
+// Salespersion problem (Finding a minimum distance between n cities)
 
-    for (let i = 2; i <= factorialInput; i++) {
-        factorialOutput *= i;
+
+
+/* ALGORITHMS */
+const prompt = require('prompt-sync')();
+const show = (o) => console.log(o);
+const range = (start, stop, step) =>
+    Array.from({ length: (stop - start) / step + 1 }, (_, i) => start + (i * step));
+
+/* MATH ALGORITHMS */
+// const computeFactorial = () => {
+//     let factorialInput = +(d.querySelector("#factorial").value);
+//     let factorialOutput = 1;
+//     let facOutput = d.querySelector("#fac_output");
+
+//     for (let i = 2; i <= factorialInput; i++) {
+//         factorialOutput *= i;
+//     }
+
+//     facOutput.innerText = "!" + factorialInput.toString() + " = " + factorialOutput.toString();
+// };
+
+// const computeFibonacci = () => {
+//     let fibonacciInput = +(d.querySelector("#fibonacci").value);
+//     let fibOutput = d.querySelector("#fib_output");
+//     let fibonacciOutput = [0, 1];
+//     let cValue = 1, pValue = 0;
+
+//     if (fibonacciInput === 1) return fibonacciOutput;
+
+//     for (; fibonacciInput > 1; fibonacciInput--) {
+//         cValue += pValue;
+//         pValue = cValue - pValue;
+
+//         fibonacciOutput.push(cValue);
+//     }
+
+//     fibOutput.innerText = "Fibonacci sequence: " + fibonacciOutput.toString();
+// };
+
+// const computePrimeF = () => {
+//     let primeFInput = +(d.querySelector("#primef"));
+//     let primeFOutput = [];
+//     if (primeFInput < 2) primeFOutput.push(0);
+
+// };
+
+/* SEARCH ALGORITHMS */
+// O(n) - linear time
+function simpleSearch(arr, item) {
+    let guess = 0;
+    for (; guess < arr.length; guess++) {
+        if (arr[guess] === item) return guess + 1;
     }
-    
-    facOutput.innerText = "!" + factorialInput.toString() + " = " + factorialOutput.toString();
-};
+    return "Incorrect value!";
+}
 
-// Fibonacci
+// O(log n) - logarithmic time
+function binarySearch(list, item) {
+    let low = 0;
+    let high = list.length - 1;
 
-const computeFibonacci = () => {
-    let fibonacciInput = +(d.querySelector("#fibonacci").value);
-    let fibOutput = d.querySelector("#fib_output");
-    let fibonacciOutput = [0, 1];
-    let cValue = 1, pValue = 0;
+    while (low <= high) {
+        let mid = Math.floor((low + high) / 2);
+        let guess = list[mid];
 
-    if (fibonacciInput === 1) return fibonacciOutput;
-
-    for (; fibonacciInput > 1; fibonacciInput--) {
-        cValue += pValue;
-        pValue = cValue - pValue;
-
-        fibonacciOutput.push(cValue);
+        if (guess === item) {
+            return mid;
+        }
+        if (guess > item) {
+            high = mid - 1;
+        } else {
+            low = mid + 1;
+        }
     }
 
-    fibOutput.innerText = "Fibonacci sequence: " + fibonacciOutput.toString();
-};
-
-// Prime Factors
-
-const computePrimeF = () => {
-    let primeFInput = +(d.querySelector("#primef"));
-    let primeFOutput = [];
-
-    if (primeFInput < 2) primeFOutput.push(0);
-
-    
-};
+    return "Incorrect value!";
+}
--- projects/psgen/psgen.js
+++ projects/psgen/psgen.js
@@ -1,23 +1,23 @@
-// Node js (prompt-sync)
+// NodeJS (prompt-sync)
 
 const prompt = require('prompt-sync')();
 
 const upperLetters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ",
-      lowerLetters = "abcdefghijklmnopqrstuvwxyz",
-      numbers = "0123456789",
-      symbols = "!@#$%^&*()_=+";
+    lowerLetters = "abcdefghijklmnopqrstuvwxyz",
+    numbers = "0123456789",
+    symbols = "!@#$%^&*()_=+";
 
 const getUpperCase = () => upperLetters[Math.floor(Math.random() * upperLetters.length)],
-      getLowerCase = () => lowerLetters[Math.floor(Math.random() * lowerLetters.length)],
-      getNumbers = () => numbers[Math.floor(Math.random() * numbers.length)],
-      getSymbols = () => symbols[Math.floor(Math.random() * symbols.length)];
+    getLowerCase = () => lowerLetters[Math.floor(Math.random() * lowerLetters.length)],
+    getNumbers = () => numbers[Math.floor(Math.random() * numbers.length)],
+    getSymbols = () => symbols[Math.floor(Math.random() * symbols.length)];
 
 console.log('Welcome to password generator!!!');
 const char = +(prompt('How many characters? ')),
-      ul = prompt('UPPER letters? (Y/N) '),
-      ll = prompt('lower letters? (Y/N) '),
-      n = prompt('Mb some numbers? (Y/N) '),
-      s = prompt('What about symbols? (Y/N) ');
+    ul = prompt('UPPER letters? (Y/N) '),
+    ll = prompt('lower letters? (Y/N) '),
+    n = prompt('Mb some numbers? (Y/N) '),
+    s = prompt('What about symbols? (Y/N) ');
 
 const generateArray = () => {
     const arr = [];
@@ -37,7 +37,7 @@ const generatePassword = () => {
         let arr = generateArray();
         password += arr
     }
-    
+
     console.log(password);
 };
 
